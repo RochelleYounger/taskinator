729e9e9865ec86168f1b2b08d4e27c0425800b71
Rochelle Younger <regnuoy321@gmail.com>
2021-12-12 09:28:22 -0500

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,9 +19,11 @@
 // taskItemEl.textContent = "hello";
 // taskItemEl;
 
-var buttonEl = document.querySelector("#save-task");
-// console.log(buttonEl);
+// var buttonEl = document.querySelector("#save-task");
+// // console.log(buttonEl);
+
 var tasksToDoEl = document.querySelector("#tasks-to-do")
+var formEl = document.querySelector("#task-form");
 
 /***First Draft of Add Task Button Functionality****
 buttonEl.addEventListener("click", function() {
@@ -33,10 +35,16 @@ buttonEl.addEventListener("click", function() {
 
 /*****OPTIMIIZED "ADD TASK" BUTTON FUNCTIONALITY*****/
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
+    console.log(event);
+
     var listItemEl = document.createElement("li");
     listItemEl.className= "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+// buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);

88f9e85651d03645ea57cabbf40cb0b53766e271
Rochelle Younger <regnuoy321@gmail.com>
2021-12-12 08:27:55 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+/**DOCUMENT OBJECT MODEL (DOM)**/
+// console.log(window.document);
+// console.dir(window.document);
+
+/****Object representation of <button>****/
+// window.document.querySelector("button");
+// console.dir(btn);
+
+/********Object targeting of textContent********/
+// document.querySelector("button").textContent;
+
+/*******Targetting Elements by ID*******/
+// document.querySelector("#save-task");
+
+/***To Create a List Element***/
+// document.createElement("li");
+
+/*Assigning Text Content To an Element*/
+// taskItemEl.textContent = "hello";
+// taskItemEl;
+
+var buttonEl = document.querySelector("#save-task");
+// console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do")
+
+/***First Draft of Add Task Button Functionality****
+buttonEl.addEventListener("click", function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}); */
+
+/*****OPTIMIIZED "ADD TASK" BUTTON FUNCTIONALITY*****/
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className= "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);

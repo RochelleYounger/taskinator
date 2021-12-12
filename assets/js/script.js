94db690c53e13a8bb1b5f35d7d07fad36d4717c7
Rochelle Younger <regnuoy321@gmail.com>
2021-12-12 10:28:14 -0500

    refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,11 @@
 // var buttonEl = document.querySelector("#save-task");
 // // console.log(buttonEl);
 
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+
+/**********************************CODE BEFORE THE REFACTOR START**********************************/
+/* var tasksToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
 
 /***First Draft of Add Task Button Functionality****
@@ -31,10 +35,10 @@ buttonEl.addEventListener("click", function() {
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}); */
+}); 
 
-/*****OPTIMIIZED "ADD TASK" BUTTON FUNCTIONALITY*****/
-var createTaskHandler = function() {
+/*****OPTIMIIZED "ADD TASK" BUTTON FUNCTIONALITY*******
+    var createTaskHandler = function() {
     event.preventDefault();
     // console.log(event);
     var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -63,4 +67,45 @@ var createTaskHandler = function() {
 };
 
 // buttonEl.addEventListener("click", createTaskHandler);
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler); */
+/**********************************CODE BEFORE THE REFACTOR END**********************************/
+
+/**********************************CODE AFTER THE REFACTOR START**********************************/
+
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var taskFormHandler = function() {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput,
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj) {
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className= "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name 
+        + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", taskFormHandler);
+
+/**********************************CODE AFTER THE REFACTOR END**********************************/
